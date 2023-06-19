37a8225222a194cdb6fd3a8765dac16c7cd66a3e
uhleesuuuh <espiritu.alisa@gmail.com>
2023-06-19 11:43:37 +0800

    first commit

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Hello NodeJS!');
+var arr = [1, 2, 3, 4];
+for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+    var i = arr_1[_i];
+    console.log(arr[i]);
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log('Hello NodeJS!')
+
+const arr: number[] = [1, 2, 3, 4]
+for (const i in arr) {
+    console.log(arr[i])
+}
